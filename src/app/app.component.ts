49a14e1e1e8c1c6dbdf44c72b7bb43c631e0feb7
jose hernandez <jose2889@gmail.com>
2017-12-03 12:07:36 -0400

    este es el primer commit de notas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  notes: FirebaseListObservable<any[]>;
+  
+    constructor(public afDB: AngularFireDatabase) {
+      this.notes = afDB.list('notas').valueChanges();
+    }
+
+  datos =  {'id':null, 'title':null, 'description':null};
+  show:boolean = false;
+  editing:boolean = false;
+
+  addNote() {
+    this.show = true;
+    
+  }
+  viewNote(nota) {
+    this.show = true;
+    this.datos = nota;
+    this.editing = true;
+
+  }
+  cancel() {
+    this.show = false;
+    
+  }
+
+  createNote() {
+    this.datos.id = Date.now();
+    this.afDB.database.ref('notas/' + this.datos.id).set(this.datos);
+    // if (this.editing)
+    //   { 
+    //     var me = this;
+    //     this.notes.forEach(function(el, i) {
+    //       if (el.id === me.datos.id ) {
+    //         me.notes[i] = me.datos;
+    //       }
+    //     }
+    //   )
+    //   me.show = false;
+    //   }
+    // else
+    // {      
+    //   this.datos.id = Date.now();
+    //   this.notes.push(this.datos);
+    //   this.show = false;
+    //   this.datos =  {'id':null, 'title':null, 'description':null};
+    // }    
+  }
+
+  // deleteNote() {
+  //   var me = this;
+  //   me.notes.forEach(function(el, i) {
+  //     if (el === me.datos ) {
+  //       me.notes.splice(i, 1)
+  //     }
+  //   })
+  //   this.show = false;
+  //   this.datos =  {'id':null, 'title':null, 'description':null};
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvj1aFWn-ht4_WC6pVlzgx79S9XrxkvNc",
+  authDomain: "app-notas-913ab.firebaseapp.com",
+  databaseURL: "https://app-notas-913ab.firebaseio.com",
+  storageBucket: "app-notas-913ab.appspot.com",
+  messagingSenderId: '721419876483'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireDatabase,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
